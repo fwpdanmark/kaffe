924279829417cbe0bebf5dbb6a37e338c70f082a
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2018-09-10 17:49:18 +0200

    Opgaven løst

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    class FlatWhite : Kaffe, Imælk
+    {
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
